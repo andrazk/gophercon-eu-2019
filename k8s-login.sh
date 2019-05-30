f04a261d71c332061ce1362e1e89ee4566c405c1
Andraz Krascek <andraz.krascek@3fs.si>
2019-05-30 14:25:23 +0100

    replace k8s_user with namespace

--- k8s-login.sh
+++ k8s-login.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-kubectl config set-cluster ws-${K8S_USER} \
+kubectl config set-cluster ws-${NAMESPACE} \
     --embed-certs=true \
     --server=https://35.205.229.158 \
     --certificate-authority=${CA_PATH}
-kubectl config set-credentials ws-${K8S_USER} --token=${TOKEN}
-kubectl config set-context ws-${K8S_USER} \
-    --cluster=ws-${K8S_USER} \
-    --user=ws-${K8S_USER} \
-    --namespace=${K8S_USER}
-kubectl config use-context ws-${K8S_USER}
+kubectl config set-credentials ws-${NAMESPACE} --token=${TOKEN}
+kubectl config set-context ws-${NAMESPACE} \
+    --cluster=ws-${NAMESPACE} \
+    --user=ws-${NAMESPACE} \
+    --namespace=${NAMESPACE}
+kubectl config use-context ws-${NAMESPACE}
